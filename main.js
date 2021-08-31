f1ed0e815818f356a2ba0a03f057e7b11cb76f92
kunlar365 <86461406+kunlar365@users.noreply.github.com>
2021-08-31 12:18:39 +0500

    added html codes

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 let box = document.getElementsByTagName('div')
 
-for (let i = 0; i < box.length; i++) {
-    setTimeout(() => {
-        box[i].style.opacity = '1'
-    }, (i+1)*1000) 
+
+function count(){
+     let i = 0
+    setInterval(() => {
+        if(i == box.length){
+            i=0;
+            for(el of box) {
+                el.style.opacity = '0';
+            }
+        }
+        box[i].style.opacity = '1';
+        i++;
+        console.log(i,box.length);
+    }, 2000);
+    
 }
+
+count()
+
